196c0e8d6c6427ca6abf2fb03b18819fcfa7d117
CHOMINJI <>
2018-12-30 18:12:59 +0900

    first commit

---
+++ TMI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TMI
+//
+//  Created by CHOMINJI on 2018. 12. 30..
+//  Copyright © 2018년 momo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
