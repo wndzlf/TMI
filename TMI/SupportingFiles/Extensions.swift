7ab853c04835a353493c36235b539110c769f3bd
CHOMINJI <>
2019-02-06 19:46:21 +0900

    Modify Extension
    Extension 오류 수정

--- TMI/SupportingFiles/Extensions.swift
+++ TMI/SupportingFiles/Extensions.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 
+
 extension UIViewController {
     class func displaySpinner(onView : UIView) -> UIView {
         let spinnerView = UIView.init(frame: onView.bounds)
@@ -216,33 +217,3 @@ extension Array where Element: Equatable {
 }
 
 
-extension UIViewController {
-    
-    func getUIImage(asset: PHAsset) -> UIImage? {
-        
-        var img: UIImage?
-        let manager = PHImageManager.default()
-        let options = PHImageRequestOptions()
-        options.version = .original
-        options.isSynchronous = true
-        manager.requestImageData(for: asset, options: options) { data, _, _, _ in
-            
-            if let data = data {
-                img = UIImage(data: data)
-            }
-        }
-        return img
-    }
-    
-    func convertImageFromAsset(asset: PHAsset) -> UIImage {
-        let manager = PHImageManager.default()
-        let option = PHImageRequestOptions()
-        var image = UIImage()
-        option.isSynchronous = true
-        manager.requestImage(for: asset, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFill, options: option, resultHandler: {(result, info) -> Void in
-            image = result!
-        })
-        return image
-    }
-
-}
